af673ad7490c5f5916651a1c73b68cfb9eae090d
George Castro <gamcastro@bol.com.br>
2023-05-03 04:15:37 -0300

    Adicionado contralador para o Get-GeoFaturaCartaoCredito

---
+++ InformacoesCartaoCredito.ps1
@@ -0,0 +1,18 @@
+﻿[CmdletBinding()]
+param(
+    [string]$Arquivo,
+
+    [string[]]$Tipo
+)
+Clear-Host
+Write-Host "                   Listando Valores Contidos na Fatura do [Mês] - [Ano]"
+Write-Host "-------------------------------------------------------------------------------------"
+Write-Host ""
+
+$ret = Get-GeoFaturaCartaoCredito -FilePath $Arquivo -Filter $Tipo 
+$ret | Format-Table @{Name = 'Categoria'; Expression = { $_.Categoria }; Width = 40 },
+@{Name = 'Valor'; Expression = { $_.'Valor Total' }; Width = 30; align = 'left'; formatstring = 'c' }
+
+Write-Host ""                    
+Write-Host "-------------------------------------------------------------------------------------"
+$ret | Measure-Object -Property 'Valor Total' -Sum | Format-Table @{Name='Total Geral';Expression={$_.SUM};align='right';formatstring='c';width=50}
---
+++ Parameters.ps1
@@ -0,0 +1,26 @@
+﻿[CmdletBinding(DefaultParameterSetName="name")]
+Param(
+    [Parameter(Position = 0,Mandatory,HelpMessage="Enter a computer name to check",ParameterSetName="name",ValueFromPipeline)]
+    [Alias("cn")]
+    [ValidateNotNullorEmpty()]
+    [string[]]$Computername,
+
+    [Parameter(Mandatory,HelpMessage="Enter the path to a text fil of computer names",ParameterSetName="file")]
+    [ValidateScript({
+        if (Test-Path $_){
+            $True
+        }else {
+            Throw "Cannot validate path $_"
+        }
+    })]
+    [ValidatePattern("\.txt$")]
+    [string]$Path,
+
+    [ValidateRange(10,50)]
+    [int]$Threshhold=25,
+
+    [ValidateSet("C:","D:","E:","F:")]
+    [string]$Drive="C:"
+
+    [switch]$Test
+)
