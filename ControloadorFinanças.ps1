8f27638c9b2c78c2f1adaa7205c9e3dc5c5ae76b
George Castro <gamcastro@bol.com.br>
2023-05-06 21:51:35 -0300

    Criado controlador para Get-GeoFaturaCartaoDeCredito

---
+++ ControloadorFinanças.ps1
@@ -0,0 +1,20 @@
+﻿[CmdletBinding()]
+Param(
+    [string]$Arquivo
+)
+Clear-Host
+Write-Host "                      Controloador Financeiro         "
+Write-Host " "
+$itensCompra = @()
+$fatura = Get-GeoFaturaCartaoCredito -FilePath $Arquivo
+$itensCompra += $fatura | Group-Object -Property ItemCompra | ForEach-Object {
+    [PSCustomObject]@{
+        'Item' = $_.Name
+        'Total' = ($_.Group | Measure-Object -Property ValorItem -Sum).Sum
+        'QtdCompras' = $_.Count
+    }
+}
+$itensCompra | Sort-Object -Property Total -Descending | 
+              Format-Table -Propert @{Name = 'Quantidade de Compras';Expression={$_.QtdCompras};Align='center';Width=15},
+                                    @{Name = 'Item';Expression={$_.Item};Align='center';Width=40},
+                                    @{Name='Valor Comprado';Expression={$_.Total};Width=10}
